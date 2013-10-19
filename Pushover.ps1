0eff085759cf20724e5f91f8fc2c81d7bf593e51
Nicholas M. Getchell <getch3028@Gmail.com>
2013-10-19 19:24:31 -0400

    Initial Commit

---
+++ Pushover.ps1
@@ -0,0 +1,23 @@
+﻿$PushoverURI = 'https://api.pushover.net/1/messages.json'
+$PushoverUserKey = 'udFKDUGUbYXm32uogzLcp4EHskkBAV'
+$PushoverAppToken = 'agAvpEEvTb36Cdo6HkV5yUq6eyNT1q'
+
+Function Send-PushoverMessage {
+param(
+    $Message = "",
+    $Title = ""
+)
+
+
+    $Parameters = @{
+        token=$PushoverAppToken
+        user=$PushoverUserKey
+        message=$Message
+        title=$Title
+
+    }
+
+    Write-Output ($Parameters | Invoke-RestMethod -Uri $PushoverURI -Method Post)
+
+}
+
