80e353aeb8e8b86e43524398f59be85cae22cbca
Nicholas M. Getchell <getch3028@gmail.com>
2013-12-08 21:53:52 -0500

    Datatype for all parameters.

--- Pushover.ps1
+++ Pushover.ps1
@@ -5,13 +5,13 @@ $Global:PreviousPushMessages = @()
 
 Function Send-PushoverMessage {
 param(
-    $Message = "",
-    $Title = "",
-    $URL = '',
-    $URLTitle = '',
+    [string]$Message = "",
+    [string]$Title = "",
+    [string]$URL = '',
+    [string]$URLTitle = '',
     [ValidateSet("-1","0","1","2")] 
     [int]$Priority = '0',
-    $Expire = '',
+    [int]$Expire = '',
     [ValidateSet("CashRegister","Bike","Bugle","Classical","Cosmic","Falling","GameLan","Incomming","Intermission","Magic","Mechanical","PianoBar","Siren","SpaceAlarm","TugBoat","Alien","Climb","Persistent","Echo","UpDown","none")]
     [string]$Sound = ''
 )
