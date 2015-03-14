2fd2b399a50dcb75b39543a5fc6453fa05b2cd5a
Nicholas Getchell <ngetchell@massasoit.mass.edu>
2015-03-14 15:19:05 -0400

    Cleaned up params and added dynamic parameters
    fixes #2 - cleaned up the look and feel of function parameters
    fixes #7 - dynamic parameters for devices
    fixes #6 - timestamp issues
    fixes #8 - get devices. used in the dynamic paramters set for devices

--- PushOverAlert.psm1
+++ PushOverAlert.psm1
@@ -29,55 +29,71 @@ param(
 Function Send-PushoverMessage {
 [cmdletbinding()]
 param(
+    [string]$token = "$(Import-Clixml "$CommandPath\pushoverapiauth.xml" | Select-Object -ExpandProperty AppToken)",
+
+    [string]$user = "$(Import-Clixml "$CommandPath\pushoverapiauth.xml" | Select-Object -ExpandProperty UserKey)",
+
     [Parameter(Mandatory=$true)]
-    [string]$Message = "",
+    [string]$message = "",
 
-    [string]$Title = "",
     
-    [string]$URL = '',
+    [string]$title,
     
-    [string]$URLTitle = '',
+    [string]$url = '',
     
-    [ValidateSet("-1","0","1","2")] 
-    [int]$Priority = '0',
+    [string]$url_title = '',
     
-    [int]$Expire = '86400',
-    
-    [DateTime]$Timestamp = '',
-    
-    [String]$Device,    
-    
+    [ValidateSet("-2","-1","1")] 
+    [int]$priority,
+
     [ValidateSet("pushover","bike","bugle","cashregister","classical","cosmic","falling","gamelan","incoming","intermission","magic","mechanical","pianobar","siren","spacealarm","tugboat","alien","climb","persistent","echo","updown","none")]
-    [string]$Sound = '',
-    
-    [int]$Retry
+    [string]$sound,
+        
+    [DateTime]$timestamp
 )
-
-    $Config = Import-Clixml "$CommandPath\pushoverapiauth.xml"
-
-    $Parameters = @{
-        token=$Config.AppToken
-        user=$Config.UserKey
-        message=$Message
-        title=$Title
-        url=$URL
-        url_title=$URLTitle
-        priority=$Priority
-        device=$Device
-        timestamp=(Get-Date $Timestamp -UFormat %s) -replace("[,\.]\d*", "")
-        expire=$Expire
-        sound=$sound.ToLower()
-        retry=$Retry
-
-    }
-
-    #Write-Output ($Parameters | Invoke-RestMethod -Uri $PushoverURI -Method Post)
-    $Results = $Parameters | Invoke-RestMethod -Uri $PushoverURI -Method Post
-    $Results | Add-Member -MemberType NoteProperty -Name DateTime -Value (Get-Date)
-    $Results | Add-Member -MemberType NoteProperty -Name Title -Value $Parameters.Title
-    $Global:PreviousPushMessages += $Results
-    Write-Output $Results
+DynamicParam {
+        $ParameterName = 'device'
+        $RuntimeDefinedParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+        $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+        $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+        $AttributeCollection.Add($ParameterAttribute) 
+        $devices = Get-PushoverUserDevices
+        $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($devices)
+        $AttributeCollection.Add($ValidateSetAttribute)
+        $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection)
+        $RuntimeDefinedParameterDictionary.Add($ParameterName, $RuntimeParameter)
+        return $RuntimeDefinedParameterDictionary
 }
+
+    BEGIN {}
+    PROCESS {}
+    END {
+        $params = @{}
+        foreach($h in $MyInvocation.MyCommand.Parameters.GetEnumerator()) {
+                $key = $h.Key
+                if ($key -match "Debug|Verbose|OutVariable|WarningVariable|OutBuffer|ErrorVariable|PipelineVariable|ErrorAction|WarningAction") {
+                    break;
+                }
+                $val = Get-Variable -Name $key | Select-Object -ExpandProperty Value
+                #Skip Automatic Variables
+                if (([String]::IsNullOrEmpty($val) -and (!$PSBoundParameters.ContainsKey($key)))) {
+                    break;
+                }
+                if ($key -eq 'timestamp') {
+                    $val = (Get-Date $val.ToUniversalTime() -UFormat %s) -Replace("[,\.]\d*", "") 
+             
+                }
+                $params[$key] = $val
+        }
+
+        $Results = $params | Invoke-RestMethod -Uri $PushoverURI -Method Post
+        $Results | Add-Member -MemberType NoteProperty -Name DateTime -Value (Get-Date)
+        $Results | Add-Member -MemberType NoteProperty -Name Title -Value $params.Title
+        $Global:PreviousPushMessages += $Results
+    }
+}
+
+
 
 Function Get-PushoverReceipt {
     param (
@@ -90,4 +106,21 @@ Function Get-PushoverReceipt {
         Invoke-WebRequest -Uri "https://api.pushover.net/1/receipts/$Receipt.json?token=$PushoverAppToken"
     }
 
+}
+
+Function Get-PushoverUserDevices {
+param(
+    [string]$token = "$(Import-Clixml "$CommandPath\pushoverapiauth.xml" | Select-Object -ExpandProperty AppToken)",
+
+    [string]$user = "$(Import-Clixml "$CommandPath\pushoverapiauth.xml" | Select-Object -ExpandProperty UserKey)"
+)
+
+
+    $Parameters = @{
+        token=$token;
+        user=$user;
+    }
+
+    $Result = $Parameters | Invoke-RestMethod -Uri 'https://api.pushover.net/1/users/validate.json' -Method Post
+    Write-Output $Result.devices
 }
